7b162117e6d7d6149c288340275bbf198ef7007a
Ник CHARON IV <niksbozhkov@gmail.com>
2024-11-19 16:03:38 +0300

    Initial project setup

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> tasks = new List<string>();
+            string command;
+
+            Console.WriteLine("Добро пожаловать в Todo List!");
+            do
+            {
+                Console.WriteLine("Введите команду (add/show/exit):");
+                command = Console.ReadLine();
+
+                if (command == "add")
+                {
+                    Console.WriteLine("Введите задачу:");
+                    string task = Console.ReadLine();
+                    tasks.Add(task);
+                    Console.WriteLine("Задача добавлена.");
+                }
+                else if (command == "show")
+                {
+                    Console.WriteLine("Ваши задачи:");
+                    foreach (var t in tasks)
+                    {
+                        Console.WriteLine(t);
+                    }
+                }
+
+            } while (command != "exit");
+        }
+    }
+}
